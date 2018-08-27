d16c3241260a5d952518bf13b610488dcc9ab621
md mia <sharifmia091@gmail.com>
2018-08-27 04:36:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -53,3 +53,18 @@ def over?(board)
     return false
   end
 end
+
+
+def winner(board)
+  index =[]
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]]= "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
